0fbf3d5c83be977c940770aa435f9ce1ff8ce2a5
Bruno Ochotorena <hackerhilt@gmail.com>
2021-10-24 17:52:41 -0300

    feat: :truck: new routes to test

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,18 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Example");
+  res.send('Hello world');
 });
+
+app.post('/addresses', (req, res) => {
+  console.log(req.body);
+  
+  res.status(200).send({'Addresses': req.body});
+})
 
 app.listen(PORT, () => {
   console.log(`Server listen in http://localhost:${PORT}`);
